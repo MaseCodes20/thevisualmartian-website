212f68e98e2fde5f73887eec33f9a9c5b5506dfe
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-02-24 14:46:17 -0500

    Footer added copyright logo and link to instagram

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,27 @@ function Footer() {
     <div className="absolute bottom-0 w-full bg-white h-[80px]">
       <div className="text-center my-5">
         <p className="text-xs">
-          Built and designed by{" "}
+          Powered by{" "}
           <a href="https://www.masecodes.com/" target="_blank">
             Mase Codes
           </a>{" "}
         </p>
-        <p className="text-xs">All rights reserved. (C)</p>
+        <div className="flex justify-center items-center">
+          <img
+            src="copyrightlogo.png"
+            alt="copyright log"
+            className="h-3 mr-1 mix-blend-darken"
+          />
+          <p className="text-xs">
+            <a
+              href="https://www.instagram.com/thevisualmartian/"
+              target="_blank"
+            >
+              The Visual Martian
+            </a>{" "}
+            , All rights reserved.
+          </p>
+        </div>
       </div>
     </div>
   );
