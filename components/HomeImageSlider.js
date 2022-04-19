21d9fe2264b54225ca4c377dd6006539f5685a6a
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2022-04-19 17:59:56 -0400

    HomeImageSlider bug fixed

--- components/HomeImageSlider.js
+++ components/HomeImageSlider.js
@@ -7,22 +7,26 @@ function HomeImageSlider({ slides }) {
     return null;
   }
 
+  console.log(slides[0]);
+
   return (
-    <div className="sliderContainer">
-      <div className="flex">
-        {HomeSliderData.map((slide) => {
-          const { image, id } = slide;
-          return (
-            <div className="relative w-[413px] h-[620px] mx-2" key={id}>
-              <Image
-                src={image}
-                alt="Portrait"
-                layout="fill"
-                objectFit="contain"
-              />
-            </div>
-          );
-        })}
+    <div className="sliderContainer ">
+      <div className="overflow-x-scroll scrollbar-hide">
+        <div className="flex justify-center items-center min-w-max ">
+          {HomeSliderData.map((slide) => {
+            const { image, id } = slide;
+            return (
+              <div className="relative w-[413px] h-[620px] mr-2" key={id}>
+                <Image
+                  src={image}
+                  alt="Portrait"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
