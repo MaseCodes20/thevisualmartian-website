4dccf1ba46cb61e371491f19470dfecc758b3412
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2026-01-23 00:48:30 -0400

    Videos updated music video section: added one new video

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -11,6 +11,11 @@ function videos() {
     "KI8xn1tsWtM"
   ];
 
+  const musicVideos = [
+    "i2dRDu9a2MU",
+    "rfa55wFnln4"
+  ]
+
   return (
     <div>
       <Head>
@@ -25,15 +30,19 @@ function videos() {
         <main className="contentContainer pt-24 pb-20 px-4">
           <section className="text-center mb-12">
             <h1 className="text-2xl font-bold mb-6">Music Videos</h1>
-            <div className="max-w-[800px] mx-auto aspect-video">
-              <iframe
-                className="w-full h-full rounded-md shadow-lg"
-                src="https://www.youtube.com/embed/rfa55wFnln4"
-                title="Featured Music Video"
-                style={{ border: 0 }}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              {musicVideos.map((id) => (
+                <div key={id} className="w-full aspect-video">
+                  <iframe
+                    className="w-full h-full rounded-sm shadow-sm"
+                    src={`https://www.youtube.com/embed/${id}`}
+                    title="Drone Footage"
+                    style={{ border: 0 }}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              ))}
             </div>
           </section>
 
