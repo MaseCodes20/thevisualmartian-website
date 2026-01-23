b6b32bdf7080e061e62b6ae7dfd5436e76b9e2f3
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2026-01-23 00:44:55 -0400

    Booking modal UI update

--- components/BookingModal.tsx
+++ components/BookingModal.tsx
@@ -1,43 +1,41 @@
-import { MouseEvent, useRef } from "react";
-import { useBookingStore } from "../store/useBookingStore";
+import { MouseEvent, useRef } from "react"
+import { useBookingStore } from "../store/useBookingStore"
+import { XCircleIcon } from "@heroicons/react/24/outline"
 
 function BookingModal() {
-  const { showModal, setShowModal } = useBookingStore();
-  
-  const modalRef = useRef<HTMLDivElement>(null);
+  const { showModal, setShowModal } = useBookingStore()
+
+  const modalRef = useRef<HTMLDivElement>(null)
 
   const closeModal = (e: MouseEvent<HTMLDivElement>) => {
     if (modalRef.current === e.target) {
-      setShowModal(false);
+      setShowModal(false)
     }
-  };
+  }
   return (
     <div>
       {showModal && (
         <div
-          className="fixed flex top-0 left-0 right-0 bottom-0 items-center justify-center h-screen bg-gray-600/60 z-20"
+          className="fixed flex top-0 left-0 right-0 bottom-0 items-center justify-center h-screen bg-gray-600/80 z-20"
           ref={modalRef}
           onClick={closeModal}
         >
-          <div className="centeredOnScreen h-[300px] w-[360px] md:h-[400px] md:w-[600px] bg-white rounded-xl shadow-xl">
+          <div className="centeredOnScreen h-75 w-90 md:h-100 md:w-150 bg-white rounded-xl shadow-xl">
             <div className="flex">
-              <img
-                src="bookModal\DSC_1779.jpg"
-                alt="Model Picture"
-                className="h-[300px] md:h-[400px] rounded-l-xl"
-              />
+              <img src="bookModal\DSC_1779.jpg" alt="Model Picture" className="h-75 md:h-100 rounded-l-xl" />
               <div className="relative w-full bg-black text-white rounded-r-xl">
+                <div className="flex items-center justify-end w-full p-3">
+                  <button className="cursor-pointer" onClick={() => setShowModal(false)}>
+                    <XCircleIcon className="h-7 hover:text-pink-500" />
+                  </button>
+                </div>
+
                 <div className="centeredOnScreen">
-                  <div className="flex-col justify-between text-center border-2 rounded-md w-[140px] p-2 border-pink-500 h-fit xl:w-60 ">
+                  <div className="flex-col justify-between text-center border-2 rounded-md w-35 p-2 border-pink-500 h-fit xl:w-60 ">
                     <h1 className="mb-2">The Visual Martian</h1>
-                    <p className="mb-7 text-sm">
-                      Capturing moments through my lens. Invest in your visual
-                    </p>
+                    <p className="mb-7 text-sm">Capturing moments through my lens. Invest in your visual</p>
                     <div className="mb-2">
-                      <a
-                        href="mailto:thevisualmartian@gmail.com"
-                        className="p-2 mb-2 bg-cyan-300 text-black hover:bg-pink-500"
-                      >
+                      <a href="mailto:thevisualmartian@gmail.com" className="p-2 mb-2 bg-cyan-300 text-black hover:bg-pink-500 rounded-md font-semibold">
                         Book now
                       </a>
                     </div>
@@ -49,7 +47,7 @@ function BookingModal() {
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default BookingModal;
+export default BookingModal
